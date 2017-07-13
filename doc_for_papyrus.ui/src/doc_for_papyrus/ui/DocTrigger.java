910efd2171ebbd0a03f555503c56bd7f31d2658d
didier <didier@biscarrosse>
2017-07-13 17:30:07 +0200

    begin/end syntax with PI grammar partial refundation.

--- doc_for_papyrus.ui/src/doc_for_papyrus/ui/DocTrigger.java
+++ doc_for_papyrus.ui/src/doc_for_papyrus/ui/DocTrigger.java
@@ -22,8 +22,9 @@ import org.xmlgen.Xmlgen;
 import org.xmlgen.notifications.Notification;
 import org.xmlgen.notifications.Notification.Gravity;
 import org.xmlgen.notifications.Notifications;
+import org.xmlgen.notifications.Notified;
 
-public class DocTrigger extends AbstractHandler {
+public class DocTrigger extends AbstractHandler implements Notified {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException 
@@ -43,7 +44,6 @@ public class DocTrigger extends AbstractHandler {
 			String notation = "'" + resources[1].getFullPath().toString().trim() + "'";
 			String uml = "'" + resources[2].getFullPath().toString().trim() + "'";
 			
-			Bundle bundle = Platform.getBundle("doc_for_papyrus.ui");
 			URL fileURL = bundle.getEntry("/config");
 			URL resolvedFileURL = null;
 			try {
@@ -54,8 +54,8 @@ public class DocTrigger extends AbstractHandler {
 			}
 			
 			String cdir = resolvedFileURL.toString();
-			String template = "'" + cdir + "docbook_template.xml'";
-			String schema = "'" + cdir + "docbook.xsd'";
+			String template = "'" + cdir + "docbook_template.orig.xml'";
+			String schema = "'http://docbook.org/xml/5.0/xsd/docbook.xsd'";
 			String info = "'" + cdir + "info.xml'";			
 			
 			IPath fullPath = resources[2].getParent().getLocation();
@@ -67,30 +67,28 @@ public class DocTrigger extends AbstractHandler {
 			
 			String[] vargs = {"uml=", uml,
 					          "notation=", notation,
-					          "info=", info,
+					          "info=", info,					          
 					          "outputDir=", outputDir,
 					          "imagesPath=", imagesPath,
 					          "--template", template,
-					       //   "--schema", schema,
+					          "--schema", schema,
 					          "--output", output,
-					          "--services", "doc_for_papyrus.ExpansionServices"};						
+					          "--services", "doc_for_papyrus.ExpansionServices",
+					          "--trace"};						
 			
+			notifications.clearNotifiedQueue();
+			notifications.add(this);
 			xmlgen.perform(vargs, getClass().getClassLoader());
-			notifyErrorLog(bundle);
 		}
 		
 		return null;
 	}
 	
-	protected void notifyErrorLog(Bundle bundle)
+	@Override
+	public void notification(Notification notification) 
 	{
-		Notifications notifications = Notifications.getInstance();		
-	    ILog log = Platform.getLog(bundle);
-	    for (Notification notification : notifications)
-	    {
-	    	IStatus status = new Status(StatusSeverity(notification.getGravity()), bundle.getSymbolicName(), notification.toString());
-	    	log.log(status);
-	    }
+    	IStatus status = new Status(StatusSeverity(notification.getGravity()), bundle.getSymbolicName(), notification.toString());
+    	log.log(status);	
 	}
 	
 	protected int StatusSeverity(Gravity gravity)
@@ -104,5 +102,8 @@ public class DocTrigger extends AbstractHandler {
 		default: 		  return IStatus.OK;
 		}
 	}
-
+	
+	private	Bundle bundle = Platform.getBundle("doc_for_papyrus.ui");
+	private Notifications notifications = Notifications.getInstance();		
+	private ILog log = Platform.getLog(bundle);
 }
